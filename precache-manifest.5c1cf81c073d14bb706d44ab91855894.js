558d710c7c69ee77d1d1476638dc978633e6c989
ReiiYuki <iguru.shou.5410070@gmail.com>
2020-03-26 16:55:22 +0000

    Deploying to gh-pages from  - 4bece683b5db9f7056beaca64910ffbbd948d54c 🚀

---
+++ precache-manifest.5c1cf81c073d14bb706d44ab91855894.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22c4542ac2acfdc7729e1b618589754b",
+    "url": "/covid-19/index.html"
+  },
+  {
+    "revision": "f6181e6a81211c179555",
+    "url": "/covid-19/static/css/2.29aa8308.chunk.css"
+  },
+  {
+    "revision": "f6181e6a81211c179555",
+    "url": "/covid-19/static/js/2.561e723a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/covid-19/static/js/2.561e723a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a85b9c8ac20f148defc",
+    "url": "/covid-19/static/js/main.e862c167.chunk.js"
+  },
+  {
+    "revision": "ab135981459951868179",
+    "url": "/covid-19/static/js/runtime-main.4bdba95a.js"
+  }
+]);
