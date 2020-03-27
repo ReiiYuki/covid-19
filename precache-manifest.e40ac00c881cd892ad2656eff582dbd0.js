ca8ce0cdc7ea73352fbcfbe983e6fdb00ff37851
ReiiYuki <iguru.shou.5410070@gmail.com>
2020-03-27 16:24:00 +0000

    Deploying to gh-pages from  - a2573d1eb375faab8d1f1513327d6bfa98ea983b 🚀

---
+++ precache-manifest.e40ac00c881cd892ad2656eff582dbd0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57a8cbd5fd9910145400898c33be8f12",
+    "url": "/covid-19/index.html"
+  },
+  {
+    "revision": "bac2e1fa01de70f83b8f",
+    "url": "/covid-19/static/css/2.29aa8308.chunk.css"
+  },
+  {
+    "revision": "bac2e1fa01de70f83b8f",
+    "url": "/covid-19/static/js/2.3c3e9753.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/covid-19/static/js/2.3c3e9753.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1633a51e8760e64871fa",
+    "url": "/covid-19/static/js/main.e3b1897c.chunk.js"
+  },
+  {
+    "revision": "ab135981459951868179",
+    "url": "/covid-19/static/js/runtime-main.4bdba95a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/covid-19/precache-manifest.4b5839778b43ce9b461b2fffff57a105.js"
+  "/covid-19/precache-manifest.e40ac00c881cd892ad2656eff582dbd0.js"
 );
 
 self.addEventListener('message', (event) => {
